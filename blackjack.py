8d2eaef6bd155394bd5b4739ab34401aebc756f4
Sigis <gabrielius.11ch@gmail.com>
2026-06-07 23:03:07 +0300

    My first Blackjack prototype game
    I have been practising python by creating a blackjack game. It is my first prototype. I will improve it later on.

---
+++ blackjack.py
@@ -0,0 +1,59 @@
+import random
+
+def blackjack():
+    deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]*4
+    random.shuffle(deck)
+    playerhand = [deck.pop(), deck.pop()]
+    dealerhand = [deck.pop(), deck.pop()]
+    print(f"Your hand: {playerhand}, Total value: {sum(playerhand)}")
+    print(f"Dealer's shows: {dealerhand[0]}")
+    playervalue = sum(playerhand)
+    dealervalue = sum(dealerhand)
+    if playervalue == 21:
+        print("You just got a blackjack!")
+    else:
+        print("Choose your next option:")
+    while playervalue < 21:
+        option = input("Press H to hit or S to stand: ")
+        if option == "H":
+            newhand = deck.pop()
+            playerhand.append(newhand)
+            print(f"Your hand: {playerhand}, Total value: {sum(playerhand)}")
+            playervalue = sum(playerhand)
+            if playervalue > 21:
+                print("Bust! Dealer wins!")
+                return
+            else:
+                continue
+        elif option == "S":
+            print("You stand.")
+            print(f"Dealer hand: {dealerhand} ")
+            break
+        else:
+            print("Wrong input!")
+    while dealervalue < 17:
+        newdealhand = deck.pop()
+        dealerhand.append(newdealhand)
+        dealervalue = sum(dealerhand)
+        print(f"Dealer hand: {dealerhand}, Total value: {sum(dealerhand)}")
+        if dealervalue > 21:
+            print("Dealer busts. You win!")
+            return
+        elif dealervalue == 21:
+            print("Dealer has got a blackjack! You lose!")
+        else:
+            continue
+    if playervalue > dealervalue:
+        print("You win!")
+    elif dealervalue > playervalue:
+        print("Dealer wins!")
+    else:
+        print("It's a tie!")
+def game():
+    while True:
+        play = input("Press y to begin playing or anything else to exit: ").lower()
+        if play == "y":
+            blackjack()
+        else:
+            exit()
+game()
